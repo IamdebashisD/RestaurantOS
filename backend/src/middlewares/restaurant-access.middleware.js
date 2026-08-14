dccbcfb6a4c2f73bdc687bb3e41587f9ad35a919
IamdebashisD <adebashisdas626@gmail.com>
2026-08-15 03:00:51 +0530

    chore: add restaurant access middleware to manage user permissions

---
+++ backend/src/middlewares/restaurant-access.middleware.js
@@ -0,0 +1,20 @@
+import ApiError from "../utils/api-error.js"
+import { findMembershipByUserAndRestaurant } from "../modules/restaurant-staff/repositories/restaurant-staff.repository.js"
+
+
+export async function requireRestaurantAccess(req, res, next) {
+    try {
+        const userId = req.user.id
+        const restaurantId = req.params.restaurantId
+
+        if (!restaurantId) throw ApiError.badRequest("Restaurant ID is required")
+
+        const membership = await findMembershipByUserAndRestaurant(userId, restaurantId)
+        if (!membership) throw ApiError.forbidden("Yuo do not have access to this restaurant")
+        
+        req.restaurantMembership = membership
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
