61070b45f810616a55bfc8e090df6b2ae90b15b2
IamdebashisD <adebashisdas626@gmail.com>
2026-08-21 01:18:08 +0530

    feat: add restaurant table controller with create and retrieve functionalities

---
+++ backend/src/modules/restaurant-tables/controllers/restaurant-table.controller.js
@@ -0,0 +1,36 @@
+import { catchAsync } from "../../../utils/catchAsync.js"
+import ApiError from "../../../utils/api-error.js"
+import ApiResponse from "../../../utils/api-response.js"
+
+import * as RestaurantTableService from "../services/restaurant-table.service.js"
+
+export const createRestaurantTableController = catchAsync(async (req, res) => {
+    const { restaurantId } = req.params
+    const { tableNumber, capacity, status } = req.body
+
+    const table = await RestaurantTableService.createRestaurantTableService({
+        restaurantId,
+        tableNumber,
+        capacity,
+        status
+    })
+
+    return ApiResponse.created(res, {
+        message: "Restaurant table created successfully",
+        data: {
+            table
+        }
+    })
+})
+
+export const getRestaurantTableController = catchAsync(async (req, res) => {
+    const { restaurantId } = req.params
+
+    const tables = await RestaurantTableService.getRestaurantTableService(restaurantId)
+
+    return ApiResponse.success(res, {
+        message: "Restaurant tables retrieved successfully",
+        data: tables
+    })
+})
+
