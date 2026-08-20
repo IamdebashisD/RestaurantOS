c695e8b5652f233337f9560e1ad867d2fbcc0096
IamdebashisD <adebashisdas626@gmail.com>
2026-08-20 23:59:43 +0530

    chore: add restaurant table repository with CRUD functions

---
+++ backend/src/modules/restaurant-tables/repositories/restaurant-table.repository.js
@@ -0,0 +1,26 @@
+import { RestaurantTable } from "../models/restaurant-table.model.js"
+
+export async function createRestaurantTable(tableData, session) {
+    return RestaurantTable.create([tableData], { session }).then(([restaurant]) => restaurant)
+}
+
+export async function findTableById(tableId, session) {
+    const query = RestaurantTable.findById(tableId)
+    if (session) query.session(session)
+    return query
+}
+
+export async function findTableByRestaurantAndNumber(restaurantId, tableNumber, session) {
+    const query = RestaurantTable.findOne({
+        restaurant: restaurantId,
+        tableNumber
+    })
+    if (session) query.session(session)
+    return query
+}
+
+export async function findTablesByRestaurant(restaurantId, session) {
+    const query = RestaurantTable.find({ restaurant: restaurantId }).sort({ tableNumber: 1 })
+    if (session) query.session(session)
+    return query
+}
