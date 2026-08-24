bb8d1de8fd7c563f2ea96615d7f55720377f4fe3
IamdebashisD <adebashisdas626@gmail.com>
2026-08-24 23:58:24 +0530

    feat: implement menu item service with create and retrieve functionalities

---
+++ backend/src/modules/restaurant-menu/services/menu-item.service.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+import ApiError from "../../../utils/api-error.js"
+
+import {
+    findRestaurantById,
+} from "../../restaurants/repositories/restaurant.repository.js"
+
+import {
+    createMenuItem,
+    findMenuItemsByRestaurant,
+} from "../repositories/menu-item.repository.js"
+
+// 1. Create Menu Item
+export async function createMenuItemService({
+    restaurantId,
+    name,
+    description,
+    price,
+    category,
+    image,
+    isAvailable
+}) {
+    const restaurant = await findRestaurantById(restaurantId, session)
+    if (!restaurant) throw ApiError.notFound("Restaurant not found")
+
+    const session = await mongoose.startSession()
+
+    try {
+        let menuItem
+        await session.withTransaction(async () => {
+            const itemData = {
+                restaurant: restaurantId,
+                name,
+                description,
+                price,
+                category,
+                image,
+                isAvailable
+            }
+
+            const result = await createMenuItem(itemData, session)
+            menuItem = Array.isArray(result) ? result[0] : result
+        })
+        return menuItem
+    } catch (error) {
+        if (error instanceof ApiError) throw error
+        throw ApiError.internal("Failed to create menu item due to a database error", error)
+    } finally {
+        await session.endSession()
+    }
+}
+
+// 2. Get All Menu Items
+export async function getRestaurantMenuItemsService(restaurantId) {
+    const restaurant = findRestaurantById(restaurantId)
+    if (!restaurant) throw ApiError.notFound("Restaurant not found")
+
+    const menuItems = await findMenuItemsByRestaurant(restaurantId)
+    
+    return menuItems
+}
+
+
