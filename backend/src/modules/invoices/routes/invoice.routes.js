85ca4d0f91701d541193947dd8ca432bb99ba360
IamdebashisD <adebashisdas626@gmail.com>
2026-09-05 23:52:19 +0530

    feat: add invoice routes for creating and retrieving invoices by various parameters

---
+++ backend/src/modules/invoices/routes/invoice.routes.js
@@ -0,0 +1,87 @@
+import { Router } from "express"
+
+import { authenticate } from "../../../middlewares/auth.middleware.js"
+import { requireRestaurantAccess } from "../../../middlewares/restaurant-access.middleware.js"
+import { requiredRole } from "../../../middlewares/role.middleware.js"
+
+import validate from "../../../middlewares/validate.middleware.js"
+import { createInvoiceDto } from "../dto/create-invoice.dto.js"
+
+import {
+    createInvoiceController,
+    getInvoiceByIdController,
+    getRestaurantInvoicesController,
+    getInvoiceByOrderController,
+    getInvoiceByNumberController,
+} from "../controllers/invoice.controller.js"
+
+
+const router = Router()
+
+/** 
+ * @route POST /api/v1/restaurants/:restaurantId/orders/:orderId/invoice 
+ * @desc Create invoice for a completed order 
+ * @access Private - OWNER / MANAGER 
+ */
+router.post(
+    "/:restaurantId/orders/:orderId/invoice",
+    authenticate,
+    requireRestaurantAccess,
+    requiredRole("OWNER", "MANAGER"),
+    validate(createInvoiceDto),
+    createInvoiceController
+)
+
+/** 
+ * @route GET /api/v1/restaurants/:restaurantId/invoices/:invoiceId 
+ * @desc Get invoice by ID 
+ * @access Private - OWNER / MANAGER */
+ router.get(
+    "/:restaurantId/invoices/:invoiceId",
+    authenticate,
+    requireRestaurantAccess,
+    requiredRole("OWNER", "MANAGER"),
+    getInvoiceByIdController
+)
+
+/** 
+ * @route GET /api/v1/restaurants/:restaurantId/invoices 
+ * @desc Get a paginated collection list of invoices for the restaurant
+ * @access Private - OWNER / MANAGER 
+ */
+router.get(
+    "/:restaurantId/invoices",
+    authenticate,
+    requireRestaurantAccess,
+    requiredRole("OWNER", "MANAGER"),
+    getRestaurantInvoicesController
+)
+
+/**
+ * @route   GET /api/v1/restaurants/:restaurantId/orders/:orderId/invoice
+ * @desc    Get a single invoice matching an active Order ID
+ * @access  Private (OWNER, MANAGER)
+ */
+router.get(
+    "/:restaurantId/orders/:orderId/invoice",
+    authenticate,
+    requireRestaurantAccess,
+    requiredRole("OWNER", "MANAGER"),
+    getInvoiceByOrderController
+)
+
+/**
+ * @route GET /api/v1/restaurants/:restaurantId/invoices/number/:invoiceNumber
+ * @desc  Get invoice by invoice number
+ * @access Private - OWNER / MANAGER
+ */
+router.get(
+    "/:restaurantId/invoices/number/:invoiceNumber",
+    authenticate,
+    requireRestaurantAccess,
+    requiredRole("OWNER", "MANAGER"),
+    getInvoiceByNumberController
+)
+
+
+export default router
