6656964ebda112b4a30493435dcbae1b0a241fc5
IamdebashisD <adebashisdas626@gmail.com>
2026-09-19 00:44:31 +0530

    fix: correct formatting of inventoryTransactionSchema declaration

--- backend/src/modules/inventory/models/inventory-transaction.model.js
+++ backend/src/modules/inventory/models/inventory-transaction.model.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const inventoryTransactionSchema  = new mongoose.Schema(
+const inventoryTransactionSchema = new mongoose.Schema(
     {
         restaurant: {
             type: mongoose.Schema.Types.ObjectId,
